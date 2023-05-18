5f4131428b42617d1f42646f81e49a644cea97fb
pkshitij291 <pkshitij291@gmail.com>
2023-05-18 12:14:29 +0530

    text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
   return (
     <div className='text-white'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p className='text-[#00df9a] font-bold p-2'>growing as front end developer</p>
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow as developer</h1>
+            <p className='text-[#00df9a] font-bold p-2'>Growing as Front End Developer</p>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow as Developer</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4 '>
                     Fast,flexible for
@@ -14,7 +14,7 @@ const Hero = () => {
                 <Typed className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2' strings={[' REACT', ' TAILWIND',' BOOTSTRAP']} typeSpeed={120} backSpeed={140} loop/>
 
             </div>
-            <p className='md:text-2xl text-xl font-bold text-gray-600'>Monitor your Projects to increase revenue revenue for multiple platforms</p>
+            <p className='md:text-2xl text-xl font-bold text-gray-600'>Monitor your Projects to increase revenue for multiple platforms</p>
             <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
         </div>
     </div>
